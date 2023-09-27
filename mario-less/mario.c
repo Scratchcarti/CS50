5081f6892c4b535bfe21f14847faa7dd925ea154
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-09-27 14:29:13 +0000

    Wed, Sep 27, 2023, 7:59 PM +05:30

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,15 +3,16 @@
 
 int main(void)
 {
-
+    int a = get_int("Height: ");
     do
     {
-    int a = get_int("Height: ");
-    }
-    while (a>1 && a<8);
-
-    for (int i =0; i<a; i++)
+        for (int i =0; i<a; i++)
     {
         printf("x")
     }
+
+    }
+    while (a>1 && a<8);
+
+
 }
