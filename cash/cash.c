a28b20cd61c87d5c757c9daaa97743fb40e089c7
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-10-16 15:33:59 +0000

    Mon, Oct 16, 2023, 9:03 PM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -1,66 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
-
-int get_cents(void);
-int calculate_quarters(int cents);
-int calculate_dimes(int cents);
-int calculate_nickels(int cents);
-int calculate_pennies(int cents);
-
 int main(void)
-{
-    // Ask how many cents the customer is owed
-    int cents = get_cents();
+{ int x=10;
+float y=5.4;
+y=x;
+printf("%f\n",y);
 
-    // Calculate the number of quarters to give the customer
-    int quarters = calculate_quarters(cents);
-    cents = cents - quarters * 25;
+x=5;
+y=9.6;
+x=y;
+printf("%d\n",x);
 
-    // Calculate the number of dimes to give the customer
-    int dimes = calculate_dimes(cents);
-    cents = cents - dimes * 10;
+x= (int)y;
+printf("%d\n",x);
 
-    // Calculate the number of nickels to give the customer
-    int nickels = calculate_nickels(cents);
-    cents = cents - nickels * 5;
-
-    // Calculate the number of pennies to give the customer
-    int pennies = calculate_pennies(cents);
-    cents = cents - pennies * 1;
-
-    // Sum coins
-    int coins = quarters + dimes + nickels + pennies;
-
-    // Print total number of coins to give the customer
-    printf("%i\n", coins);
-}
-
-int get_cents(void)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_quarters(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_dimes(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_nickels(int cents)
-{
-    // TODO
-    return 0;
-}
-
-int calculate_pennies(int cents)
-{
-    // TODO
-    return 0;
 }
