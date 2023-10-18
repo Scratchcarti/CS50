267e09af384bbab5eea6eb829a5130ed58a2dcc8
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-10-18 02:57:23 +0000

    Wed, Oct 18, 2023, 8:27 AM +05:30

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -11,9 +11,9 @@ int main(void)
     }
     while (a > 8 || a < 1);
 
-  for (int i = 0; i < a; i++)
-  {
-    for (int j = 0; j < i; j++)
+  for (int i = 0;i < a; i++)
+  { int j = 0;
+    for (j < a; j++)
     {
       printf(" ");
 
