b97e9024b5680cf94f4019edf35ec210fa63128f
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-10-20 13:11:45 +0000

    Fri, Oct 20, 2023, 6:41 PM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int get_cents(void);
-int calculate_quarters(int q);
+int calculate_quarters(int cents);
 int calculate_dimes(int cents);
 int calculate_nickels(int cents);
 int calculate_pennies(int cents);
