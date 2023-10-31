a2f8131f806a255e011744b64374d6758262bd95
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-10-31 18:01:49 +0000

    Tue, Oct 31, 2023, 11:31 PM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -123,9 +123,9 @@ int main(int argc, string argv[])
     }
     for (int i = 0; i < voter_count; i++ )
     {
-        for (int j = 0; i < candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
-            printf("%i %i %i\n", i, j, preferences[i][j]);
+            printf("%i %i \n", i, j, preferences[i][j]);
         }
         printf ("\n");
     }
