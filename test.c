c5419bdcd344d0990ef3eb7f9738293e30f9194f
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-11-08 14:29:40 +0000

    Wed, Nov 8, 2023, 7:59 PM +05:30

--- test.c
+++ test.c
@@ -1,3 +1,29 @@
 #include <stdio.h>
 int main (void)
-{   
+{   int a,b,c;
+    char gang;
+    scanf("%c %d %d", &gang, &a, &b );
+
+    switch (gang)
+    {
+        case '+': c = a + b; printf("Result = %d\n", c);
+        case '-': c = a - b; printf("Result = %d\n", c);
+        case '*': c = a * b; printf("Result = %d\n", c);
+        case '/': c = a / b; printf("Result = %d\n", c);
+        case '%': c = a % b; printf("Result = %d\n", c);
+        default : printf("invalid arithematic operation\n");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
