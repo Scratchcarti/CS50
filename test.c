82a059163df5718f9ce5e47fbf659f7f90075760
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-11-08 14:06:35 +0000

    Wed, Nov 8, 2023, 7:36 PM +05:30

--- test.c
+++ test.c
@@ -1,15 +1,3 @@
 #include <stdio.h>
 int main (void)
-{   int a,num,rem,sum=0;
-    scanf("%d", &a);
-    while (true)
-    {
-        num = a % 10;
-        sum = sum + num;
-        a = a/10;
-        if (a ==0)
-        {break;}
-        
-
-    }
-}
+{   
