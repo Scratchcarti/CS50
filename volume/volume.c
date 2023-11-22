73fa27c11c1bf1bc200893e008f73d76a370f8a6
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-11-22 15:21:23 +0000

    Wed, Nov 22, 2023, 8:51 PM +05:30

--- volume/volume.c
+++ volume/volume.c
@@ -34,7 +34,11 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    
+
+    uint8_t header[HEADER_SIZE];
+
+    fread(header,)
+
 
     // TODO: Read samples from input file and write updated data to output file
 
