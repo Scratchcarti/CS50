73b15c7caf5a36e0a00ebe80db726f54854775f0
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-11-30 17:20:23 +0000

    Thu, Nov 30, 2023, 10:50 PM +05:30

--- test.c
+++ test.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
-#include <math.h>
-int main(void)
-
-{
-    int a,b,p=0;
-    scanf("%d %d", &a,&b);
-
-    for (int i =0; i < b+1; i++)
-    {
-        p = p + pow (a, i);
-
-    }
-
-printf("%i\n", p);
+#include <stdlib.h>
 
 
 
@@ -33,5 +20,3 @@ printf("%i\n", p);
 
 
 
-
-}
