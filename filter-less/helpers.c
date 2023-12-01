9456ba2c4a336d3502c185d4c13d2388b92cdce8
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-12-01 07:23:44 +0000

    Fri, Dec 1, 2023, 12:53 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,11 +3,11 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-   for (int i = 0, i < height, i++)
+   for (int i = 0; i < height, i++)
    {
-    for (int j = 0, j < width, j++)
+    for (int j = 0; j < width, j++)
     {
-         image[height][width] = (image[height][width].rgbtRed + image[height][width].rgbtGreen + image[height][width].rgbtBlue)/3.0
+         image[height][width] = (image[height][width].rgbtRed + image[height][width].rgbtGreen + image[height][width].rgbtBlue)/3.0;
     }
    }
 
