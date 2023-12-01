26e9edb834a5aad2db233fa01b5827ca2f8c8b77
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-12-01 10:21:15 +0000

    Fri, Dec 1, 2023, 3:51 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -46,7 +46,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            a[i][j] = image [i][j];
+            image[i][j] = a[i][j];
             image[i][j] = image[i][width-(j-1)];
         }
     }
