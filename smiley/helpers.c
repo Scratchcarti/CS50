e93b5f7223daed55fc8b6b489e3ac9fa3608c996
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-12-01 06:16:36 +0000

    Fri, Dec 1, 2023, 11:46 AM +05:30

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,9 +2,9 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int i = 0, n = height; i < n, i++)
+    for (int i = 0; i<height, i++)
     {
-        for (int j=0; i<width, j++)
+        for (int j=0; j<width, j++)
         {
             if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
             {
