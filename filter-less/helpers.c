3cb24a013d73755dd5fd28bff72c6a6427023a89
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2023-12-01 09:39:37 +0000

    Fri, Dec 1, 2023, 3:09 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -15,13 +15,28 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtRed = k;
         }
     }
-    
+
     return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+     for (int i = 0; i < height; i++)
+    {   int a,b,c;
+        int k;
+        for (int j = 0 ; j < width; j++)
+        {
+            a =image[i][j].rgbtBlue, b = image[i][j].rgbtGreen, c = image[i][j].rgbtRed;
+
+
+            image[i][j].rgbtBlue = .272 * c + .534 * b + .131 * a;
+            image[i][j].rgbtGreen = .349 * c + .686 * b + .168 * a;
+            image[i][j].rgbtRed = .393 * c + .769 * b + .189 * a
+        }
+    }
+
+    return;
     return;
 }
 
