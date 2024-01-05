b23a608674a6c64c9917f9f35276ed41060f07a4
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-01-05 18:28:46 +0000

    Fri, Jan 5, 2024, 11:58 PM +05:30

--- pjt1/pt2.c
+++ pjt1/pt2.c
@@ -14,14 +14,23 @@ int main (void)
 exc[1].code = "INR"; exc[1].rate = 1;
 printf("Currency to convert FROM:     ");
 scanf("%s", tempfrom);
+
 printf("Currency to convert TO:     " );
+
 scanf("%s", tempto);
+
 for (int i =0; i < 192; i++)
 {
 if (strcmp(tempto,exc.code[i])==0)
+{
+	printf("Enter the amount to be converted:           ");
+         scanf("%s", amount);
 }
-printf("Enter the amount to be converted:           ");
-scanf("%s", amount);
+
+}
+
+
+
 
 
 }
