9a4afba399c00c2012aac1fcb83ddef473e98b67
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-01-05 18:20:51 +0000

    Fri, Jan 5, 2024, 11:50 PM +05:30

--- pjt1/pt2.c
+++ pjt1/pt2.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main (void)
 {
 	struct
-	{	char code[4];
-		float rate;
-		
-
+	{
+	char code[4];
+	float rate;
 	}
+	currency;
+
+	struct currency exc[192];
+exc[1].code = "INR"; exc[1].rate = 1;
+
+
 
 }
