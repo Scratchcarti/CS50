22e2d2fa20aa24186402d9173f053de950ad5cbd
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-11 10:58:21 +0000

    Sun, Feb 11, 2024, 4:28 PM +05:30

--- testss/test.c
+++ testss/test.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-  FILE *input = fopen("file.txt", "r");
-  FILE *output = fopen("PDF.txt","w");
-  char c[16];
-
- while(fread(&c, 8, 1, input))
-    {
-        fwrite(&c,8, 1, output);
-    }
 
 
-  return 0;
+int x;
 
-}
+char c = "Z";
+
+x = 'c';
+
+printf("%d",x);
 
 
 
