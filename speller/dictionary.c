af35504f2c3ced72516248d038d22b58f8a69510
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-13 09:39:04 +0000

    Tue, Feb 13, 2024, 3:09 PM +05:30

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -120,7 +120,27 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
-    
+    for (int i =0; i < N; i++)
+    {
+    node *cursor = table[N];
 
-    return false;
+    node *temp = cursor;
+
+    while (cursor != NULL)
+
+    {
+        cursor = cursor -> next;
+
+        free(temp);
+
+        temp = cursor;
+
+    }
+
+
+
+
+
+    }
+    return true;
 }
