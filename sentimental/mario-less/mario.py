6cf2a38eb95e27afd2cb6404a63df9311f4edf4e
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-14 15:33:38 +0000

    Wed, Feb 14, 2024, 9:03 PM +05:30

--- sentimental/mario-less/mario.py
+++ sentimental/mario-less/mario.py
@@ -4,40 +4,26 @@ def inp():
     height = get_int("Height: ")
     return height
 
+
 def main():
 
     h = inp()
 
-    while (h > 8 or h < 1 ):
+    while (h > 8 or h < 1):
 
         h = inp()
 
-    for i in range(0,h,1):
+    for i in range(0, h, 1):
 
-        for j in range(h-i-1,0,-1):
+        for j in range(h-i-1, 0, -1):
 
-            print(" ",end="")
+            print(" ", end="")
 
-        for k in range(i,h,1):
+        for k in range(0, i+1, 1):
 
-            print("#",end="")
+            print("#", end="")
 
         print()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 main()
-
