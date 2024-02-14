8987685b68b2260d616d0c895d5707b47410beec
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-14 15:19:10 +0000

    Wed, Feb 14, 2024, 8:49 PM +05:30

--- sentimental/mario-less/mario.py
+++ sentimental/mario-less/mario.py
@@ -2,18 +2,27 @@ from cs50 import get_int
 
 def inp():
     height = get_int("Height: ")
+    return height
 
 def main():
 
     inp()
 
-    if n > 8 or n < 1 :
-        
+    while (height > 8 or height < 1 ):
 
-    else :
+        inp()
 
-        for i in range(0,n,1):
-            print(f"{i}")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
