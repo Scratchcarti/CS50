962e0901ad298d49622a50696974464713ff81e5
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-15 10:12:55 +0000

    Thu, Feb 15, 2024, 3:42 PM +05:30

--- sentimental/cash/cash.py
+++ sentimental/cash/cash.py
@@ -6,10 +6,56 @@ def get_money():
 
     while(money < 0):
 
-        money = get_int("Change Owed ")
+        money = get_float("Change Owed ")
 
+    return money*100
+
+def quaters(money):
+
+    money = money/25
     return money
 
+def dimes(money):
+
+    money = money/10
+    return money
+
+def nickels(money):
+
+    money = money/5
+    return money
+
+def pennies(money):
+    money = money/1
+    return money
+
+
+def main():
+
+    money = get_money()
+
+    quaters = quaters(money)
+
+    money = money - quaters * 25
+
+    dimes = dimes(money)
+
+    money = money - dimes * 10
+
+    nickels = nickels(money)
+
+    money = money - nickels * 5
+
+    pennies = pennies(money)
+
+    money = money - pennis * 1
+
+    coins = quaters + dimes + nickels + pennies
+
+    print("f{coins}")
+
+
+main()
 
 
 
