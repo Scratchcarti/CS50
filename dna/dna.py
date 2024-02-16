94097de0c95badf3b41455f2726c9b5ad5c3c0d5
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-16 18:32:46 +0000

    Sat, Feb 17, 2024, 12:02 AM +05:30

--- dna/dna.py
+++ dna/dna.py
@@ -6,47 +6,47 @@ from sys import argv
 
 def main():
 
-   # TODO: Check for command-line usage
+import csv
+from sys import argv
+
+def main():
+
+    # TODO: Check for command-line usage
     if len(argv) != 3:
         print("Incorrect # of inputs")
         exit()
 
     # TODO: Read database file into a variable
     with open(argv[1]) as e:
-        reader = csv.reader(e)
+        reader = csv.DictReader(e)
         database = list(reader)
-        print(f"{database[0]}")
+
     # TODO: Read DNA sequence file into a variable
     with open(argv[2]) as f:
         sequence = f.read()
 
     # TODO: Find longest match of each STR in DNA sequence
-    matches = []
-    for i in range(1, len(database[0])):
-        matches.append(longest_match(sequence, database[0][i]))
-
+    matches = {}
+    for i in database[0]:
+        matches[i] = (longest_match(sequence, i))
 
     # TODO: Check database for matching profiles
     suspect = 'No Match'
-    suspect_counter = 0
+    suspect_counter = 1
 
-    for i in range(1, len(database)):
-        for j in range(len(matches)):
-            #special note, the database is all strings, so int() is required to
-            #convert from string to int
-            if matches[j] == int(database[i][j+1]):
+    for i in range(len(database)):
+        for j in matches:
+            if str(matches[j]) == database[i][j]:
                 suspect_counter += 1
-
         if suspect_counter == len(matches):
-            # We've got the suspect!  No need to continue.
-            suspect = database[i][0]
+            suspect = database[i]['name']
             break
         else:
-            suspect_counter = 0
+            suspect_counter = 1
+
     print(suspect)
 
     return
-
 
 
 def longest_match(sequence, subsequence):
