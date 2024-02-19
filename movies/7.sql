599c685687db393e60fb69c32f48b018f9f55f8d
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-19 16:06:15 +0000

    Mon, Feb 19, 2024, 9:36 PM +05:30

--- movies/7.sql
+++ movies/7.sql
@@ -1,7 +1,8 @@
-SELECT title,rating
+SELECT (SELECT title
+FROM movies
+ORDER BY title),rating
 FROM movies
 JOIN ratings on movies.id = ratings.movie_id
 WHERE year = 2010
-ORDER BY title
 ORDER BY rating
 ;
