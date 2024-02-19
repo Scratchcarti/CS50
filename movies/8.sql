0836121d1f5bc7b34fc4a702d279e840c8a24d21
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-19 16:35:27 +0000

    Mon, Feb 19, 2024, 10:05 PM +05:30

--- movies/8.sql
+++ movies/8.sql
@@ -1,6 +1,6 @@
 SELECT people.name
-FROM movies
-INNER JOIN people on stars.person_id = people.id
-LEFT JOIN stars on movies.id = stars.movie_id
+FROM stars
+JOIN movies on stars.movie_id = movies.id
+JOIN people on stars.person_id = people.id
 WHERE movies.title = "TOY STORY"
 ;
