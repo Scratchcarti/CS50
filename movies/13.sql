ccc83328b1861fee0fdc3f49c20d09b1b3f44172
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-20 15:53:30 +0000

    Tue, Feb 20, 2024, 9:23 PM +05:30

--- movies/13.sql
+++ movies/13.sql
@@ -2,4 +2,11 @@ SELECT *
 FROM stars
 JOIN movies on stars.movie_id = movies.id
 JOIN people on stars.person_id = people.id
-WHERE people.name = 'Bradley Cooper'
+WHERE people.name = 'Bradley Cooper' AND stars.movie_id in
+(SELECT stars.movie_id
+FROM stars
+JOIN movies on stars.movie_id = movies.id
+JOIN people on stars.person_id = people.id
+WHERE people.name = 'Jennifer Lopez'
+)
+;
