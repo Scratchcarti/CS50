03feca1e3aa713d2a7fa63e36af2958710b7712f
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-20 17:39:23 +0000

    Tue, Feb 20, 2024, 11:09 PM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -25,6 +25,11 @@ WHERE month = 7 AND day = 28 AND year = 2023 AND transcript LIKE '%bakery%';
 163 | Raymond | 2023 | 7     | 28  | As the thief was leaving the bakery, they called someone who talked to them for less than a minute. In the call, I heard the thief say that they were planning to take the earliest flight out of Fiftyville tomorrow. The thief then asked the person on the other end of the phone to purchase the flight ticket. |
 */
 
+-- bakery secuity logs
+SELECT *
+FROM bakery_security_logs
+WHERE month = 7 AND day = 28 AND year = 2023 AND hour = 10 AND minute > 15
+;
 
 
 
