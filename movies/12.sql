946fee81f647941e04fb65206542046796461913
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-20 15:28:21 +0000

    Tue, Feb 20, 2024, 8:58 PM +05:30

--- movies/12.sql
+++ movies/12.sql
@@ -2,5 +2,15 @@ SELECT (movies.title)
 FROM stars
 JOIN movies on stars.movie_id = movies.id
 JOIN people on stars.person_id = people.id
-WHERE people.name = 'Jennifer Lawrence' AND stars.person = 'Bradley Cooper'
+WHERE stars.person_id = (
+SELECT id
+FROM people
+WHERE name = 'Jennifer Lawrence'
+)
+AND  stars.person_id =
+(
+SELECT id
+FROM people
+WHERE name = 'Bradley Cooper'
+)
 ;
