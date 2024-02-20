789230ac6a828ae71cb12b708b2331ef70b80c8d
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-20 15:07:11 +0000

    Tue, Feb 20, 2024, 8:37 PM +05:30

--- movies/11.sql
+++ movies/11.sql
@@ -4,4 +4,6 @@ JOIN movies on stars.movie_id = movies.id
 JOIN people on stars.person_id = people.id
 JOIN ratings on stars.movie_id = ratings.movie_id
 WHERE people.name = 'Chadwick Boseman'
-ORDER BY ratings.rating
+ORDER BY ratings.rating DESC
+LIMIT 5
+;
