6a3bcd1e95cc69fe972dc6578bb70feb9de9e5a0
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-20 16:13:50 +0000

    Tue, Feb 20, 2024, 9:43 PM +05:30

---
+++ movies/13.sql
@@ -0,0 +1,15 @@
+SELECT people.name
+FROM stars
+JOIN movies on stars.movie_id = movies.id
+JOIN people on stars.person_id = people.id
+
+WHERE stars.movie_id IN
+(
+SELECT stars.movie_id
+FROM stars
+JOIN movies on stars.movie_id = movies.id
+JOIN people on stars.person_id = people.id
+WHERE people.name = 'Kevin Bacon'
+)
+AND people.name != 'Kevin Bacon'
+;
