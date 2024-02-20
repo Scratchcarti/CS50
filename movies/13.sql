4f78efe2caee7adb899c6c6c358e0610afcbbf97
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-02-20 16:16:37 +0000

    Tue, Feb 20, 2024, 9:46 PM +05:30

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,4 @@
-SELECT people.name
+SELECT (DISTINCT people.name)
 FROM stars
 JOIN movies on stars.movie_id = movies.id
 JOIN people on stars.person_id = people.id
@@ -9,7 +9,7 @@ SELECT stars.movie_id
 FROM stars
 JOIN movies on stars.movie_id = movies.id
 JOIN people on stars.person_id = people.id
-WHERE people.name = 'Kevin Bacon'
+WHERE people.name = 'Kevin Bacon' AND people.birth = (SELECT birth FROM people WHERE name = 'Kevin Bacon')
 )
 AND people.name != 'Kevin Bacon'
 ;
