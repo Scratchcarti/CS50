137d42e809731d472eb1bcb4a4a275a2029f90ac
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-05-17 01:22:20 +0000

    Fri, May 17, 2024, 6:52 AM +05:30

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -114,7 +114,7 @@ def shortest_path(source, target):
                     path = []
                     pnode = child
                     while pnode.parent is not None:
-                        path.append((pnode.state,pnode.actor))
+                        path.append((pnode.state,pnode.action))
                         pnode = pnode.parent
                     path.reverse()
                     return path
