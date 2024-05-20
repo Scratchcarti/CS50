b002c494aabd05984f2ab85cc4880ff98b40056a
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-05-20 07:44:19 +0000

    Mon, May 20, 2024, 1:14 PM +05:30

--- testss/class1.py
+++ testss/class1.py
@@ -212,11 +212,8 @@ if len(sys.argv) != 2:
     sys.exit("Usage: python maze.py maze.txt")
 
 m = Maze(sys.argv[1])
-print("Maze:")
-m.print()
 print("Solving...")
 m.solve()
 print("States Explored:", m.num_explored)
-print("Solution:")
-m.print()
+print("Solved")
 m.output_image("maze.png", show_explored=True)
