beb4e9455a56f566c23db2d23a531a40732ecda6
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-05-29 04:12:29 +0000

    Wed, May 29, 2024, 9:42 AM +05:30

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -116,7 +116,11 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if (len(self.cells) != self.count):
+            return self.cells
+        else:
+            return set()
+
 
     def mark_mine(self, cell):
         """
