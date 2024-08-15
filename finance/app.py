3076a57dae0deb75f881e66642a8659492c6cb4c
Scratchcarti <139873457+Scratchcarti@users.noreply.github.com>
2024-08-15 06:09:51 +0000

    Thu, Aug 15, 2024, 11:39 AM +05:30

--- finance/app.py
+++ finance/app.py
@@ -113,7 +113,12 @@ def quote():
 def register():
     """Register user"""
     if request.method == "POST":
-        
+
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        try:
+            
 
 
     return apology("TODO")
